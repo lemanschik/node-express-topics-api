8b019c3e2368a508ddfa8d48787e9e8db1b635ba
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-12-08 07:36:04 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const caller = {};
+const topicsByClassifier = [{ domain: "ad-tech-1.other.stld.tld" }, { domain: "ad-tech-1.other" }];
+const user = {};
+const epochs = Object.defineProperty([{},{},{}], "length", { writable: false });
+
+
