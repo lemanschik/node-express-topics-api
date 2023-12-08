1c4f810f5c4b822798b24d75f2cf34d2ac86ddae
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-12-08 16:50:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const caller = {};
 const ifYouAreTopSite = false; // top 50k
 const topicsByClassifier = ifYouAreTopSite ? [/** Topic Overrides **/] : [{ domain: "ad-tech-1.other.stld.tld" }, { domain: "ad-tech-1.other" }] ;
 
-const users = [{}];
-// our caller gets up to 3 epochs that return none determenistic topics. out of our observation pool.
-const epochs = Object.defineProperty([{},{},{}], "length", { writable: false });
+const users = [{
+  // our caller gets up to 3 epochs that return none determenistic topics. out of our observation pool.
+  epochs: Object.defineProperty([{},{},{}], "length", { writable: false });
+}];
+
 
 const router = { 
   caller(req,res) => {
