c5fea4fa7a0e52a795a33458a6940292e63a9a35
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-12-08 07:45:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const caller = {};
+// we are ad-tech-1 running on user that is visitor of one of our clients news-1.sample news-2.sample
+// clients do embedded our caller.
 const topicsByClassifier = [{ domain: "ad-tech-1.other.stld.tld" }, { domain: "ad-tech-1.other" }];
-const user = {};
+const users = [{}];
+// our caller gets up to 3 epochs that return none determenistic topics. out of our observation pool.
 const epochs = Object.defineProperty([{},{},{}], "length", { writable: false });
 
+const router = { 
+  caller(req,res) => {
+    // expects getting called via get with some additinal optinal headers
+    // returns html including callerjs
+    // sets 
+  },
+  topics(req,res) => {
+    // gets called with request and topic responses returns classified content.
+  }
+}
 
