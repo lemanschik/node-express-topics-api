fbbc27b64ecc30b86576beae74af2353e18f23b7
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-12-08 07:47:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const caller = {};
 // we are ad-tech-1 running on user that is visitor of one of our clients news-1.sample news-2.sample
 // clients do embedded our caller.
-const topicsByClassifier = [{ domain: "ad-tech-1.other.stld.tld" }, { domain: "ad-tech-1.other" }];
+const ifYouAreTopSite = false; // top 50k
+const topicsByClassifier = ifYouAreTopSite ? [/** Topic Overrides **/] : [{ domain: "ad-tech-1.other.stld.tld" }, { domain: "ad-tech-1.other" }] ;
+
 const users = [{}];
 // our caller gets up to 3 epochs that return none determenistic topics. out of our observation pool.
 const epochs = Object.defineProperty([{},{},{}], "length", { writable: false });
